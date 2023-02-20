0e1a067e9ccdb599745ea1823aa0b09e6e1ab335
sna0e <80393332+sna0e@users.noreply.github.com>
2023-02-21 00:03:56 +0900

    Delete Find_Kim.py

--- Find_Kim.py
+++
@@ -1,8 +0,0 @@
-def solution(seoul):
-    answer = ''
-    
-    for idx, name in enumerate(seoul):
-        if name == 'Kim':
-            answer += "김서방은 " + str(idx) + "에 있다"
-    
-    return answer
